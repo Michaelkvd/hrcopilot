ce1977b8510f13a88b5d693e50c98895d1ceca8d
Michaelkvd <maikelvdkasteele2001@hotmail.com>
2025-07-10 22:24:32 +0200

    Delete utils directory

--- utils/legal_sources.py
+++
@@ -1,4 +0,0 @@
-def check_legality(document):
-    if "beëindigingsovereenkomst" in document.lower():
-        return "Let op: controleer of aan alle vereisten voor een VSO is voldaan, zoals bedenktijd en transitievergoeding."
-    return "Document is juridisch niet direct risicovol, maar laat altijd controleren door een jurist."
