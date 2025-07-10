750c208314ca0cde6ea76aa21be46febef143624
Michaelkvd <maikelvdkasteele2001@hotmail.com>
2025-07-10 22:06:07 +0200

    Delete hr_copilot directory

--- hr_copilot/utils/prompt_templates.py
+++
@@ -1,2 +0,0 @@
-verzuimanalyse_prompt = """Voer een diepgaande analyse uit op de opgegeven verzuimdata.
-Signaleer patronen, trends en risico's. Geef concrete aanbevelingen ter verbetering van het verzuimbeleid."""
